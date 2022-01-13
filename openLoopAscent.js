ff397838e7300b6a9c52bf01a59ea6dbb03d1575
mpipess <n1k1shiden@gmail.com>
2022-01-12 20:53:21 -0500

    Fix implementation of thrust and pressure

--- openLoopAscent.js
+++ openLoopAscent.js
@@ -5,12 +5,11 @@ var g = 9.8; //gravitational acceleration (m/s^2)
 
 //Sim parameters and data
 var t = 0; //time (sec)
-var velTheta = 90; //angle of velocity vector from horizontal (degrees)
+var velTheta = Math.PI / 2; //angle of velocity vector from horizontal (degrees)
 var vertSigma = 0; //vertical velocity summed from vertical acceleration (m)
 var horizSigma = 0; //horizontal velocity summed from horizontal acceleration (m)
 var altSigma = 0; //altitude summed from vertSigma (m)
 var frequency = 5; //number of measurements per second (Hz)
-var pressure = 101325*(1-(2.25577*10**-5)*altSigma)**5.25588; //atmospheric pressure at altitude (Pa)
 
 //Rocket data
 var wetMass = 9714; //mass with fuel load (kg)
@@ -18,8 +17,22 @@ var dryMass = 3122; //mass without fuel load (kg)
 var burnTime = 142; //burn duration (sec)
 var vacThrust = 123600; //thrust produced in a vacuum (N)
 var slThrust = 112900; //thrust produced at sea level (N)
-var thrust = vacThrust-(vacThrust-slThrust)*(pressure/101325); //thrust at atmospheric pressure (N)
 var burnRate = (wetMass - dryMass) / burnTime; //fuel burn rate (kg/s)
+
+//atmospheric pressure at altitude (Pa)
+function pressure() {
+    if (altSigma < 44330) {
+        return 101325*(1-(2.25577*10**-5)*altSigma)**5.25588;
+    }
+    else {
+        return 0;
+    }
+}
+
+//thrust at atmospheric pressure (N)
+function thrust() {
+    return vacThrust-(vacThrust-slThrust)*(pressure()/101325);
+}
 
 //mass at given time t (kg)
 function mass(t) {
@@ -28,17 +41,17 @@ function mass(t) {
 
 //total acceleration prior to gravity loss at given time t (m/s^2)
 function a0(t) {
-    return thrust / mass(t);
+    return thrust() / mass(t);
 }
 
 //vertical component of acceleration including gravity loss at given time t (m/s^2)
 function av(t) {
-    return (a0(t) * (Math.sin(velTheta * degToRad))) - g;
+    return a0(t) * Math.sin(velTheta) - g;
 }
 
 //horizontal component of velocity at given time t (m/s^2)
 function ah(t) {
-    return (a0(t) * (Math.cos(velTheta * degToRad)));
+    return a0(t) * Math.cos(velTheta);
 }
 
 //sum vertical acceleration for velocity at given time t (m/s)
@@ -55,28 +68,28 @@ function sumHorizAcc(t) {
 
 //arctangent sums for angle of velocity vector from horizontal (degrees)
 function velTheta1(t) {
-    return Math.atan(sumVertAcc(t) / sumHorizAcc(t)) * radToDeg;
+    return Math.atan(sumVertAcc(t) / sumHorizAcc(t));
 }
 
 var simOutput = "";
 //simulates ascent for burn time length
 while (t < 9) {
     velTheta = velTheta1(t);
-    console.log(velTheta);
+    console.log(velTheta * radToDeg);
     altSigma += (vertSigma / frequency);
     t += 1 / frequency;
 
-    simOutput += velTheta.toString() + ", "
+    simOutput += (velTheta * radToDeg).toString() + ", "
 }
-velTheta = 89.9;
+velTheta = (8985 * Math.PI)/18000;
 console.log("Pitchover " + vertSigma);
 while (t < 143) {
     velTheta = velTheta1(t);
-    console.log(velTheta);
+    console.log(velTheta * radToDeg);
     altSigma += (vertSigma / frequency);
     t += 1 / frequency;
 
-    simOutput += velTheta.toString() + ", "
+    simOutput += (velTheta * radToDeg).toString() + ", "
 }
 
 //calculate altitude at burnout -- sum vertical acceleration (m)
@@ -86,8 +99,8 @@ console.log("Altitude at cutoff: " + altSigma)
 //alt to apogee: vf=0, a=g, vi=sumVertAcc; vf^2=vi^2+2ady; dy=-vi^2/2a
 //total alt of apogee: dy+altitude
 console.log("Vertical speed at cutoff: " + vertSigma);
-console.log("Total speed at cutoff: " + vertSigma/Math.sin(velTheta*degToRad));
+console.log("Total speed at cutoff: " + vertSigma/Math.sin(velTheta));
 console.log("Alt to apogee: " + (vertSigma**2)/(2*g));
 console.log("Apogee altitude: " + ((vertSigma**2)/(2*g) + altSigma));
 
-console.log(simOutput);
+//console.log(simOutput);
