b22b7e289f175171a5526c1ce163e9ebe72bb0d3
Michael Piper <n1k1shiden@gmail.com>
2022-02-02 13:57:55 -0500

    Add kOS file and OOP sim file

---
+++ OpenLoopAscent.ks
@@ -0,0 +1,119 @@
+
+//Sim parameters and data
+set g to 9.8. //gravitational acceleration (m/s^2)
+set t to 0. //time (sec)
+set velTheta to 90. //angle of velocity vector from horizontal (degrees)
+set vertVelocity to 0. //vertical velocity summed from vertical acceleration (m)
+set horizVelocity to 0. //horizontal velocity summed from horizontal acceleration (m)
+set myAltitude to 0. //altitude summed from vertVelocity (m)
+set frequency to 20. //number of measurements per second (Hz)
+
+//Rocket data
+set wetMass to 10000. //mass with fuel load (kg)
+set dryMass to 3000. //mass without fuel load (kg)
+set burnTime to 150. //burn duration (sec)
+set vacThrust to 125000. //thrust produced in a vacuum (N)
+set slThrust to 115000. //thrust produced at 1 atm (N)
+set burnRate to (dryMass - wetMass) / burnTime. //fuel burn rate (kg/s)
+set pitchAlt to 20. //Altitude of pitchover (m)
+set pitchAngle to 89.983. //Angle of pitchover (degrees)
+
+//Returns atmospheric pressure (Pa) at altitude
+declare function pressure {
+    if (myAltitude < 44330) {
+        return 101325 * (1 - (2.25577 * 10^(-5)) * myAltitude)^5.25588.
+    }
+    else {
+        return 0.
+    }
+}
+
+//Returns thrust (N) at pressure
+declare function thrust {
+    return vacThrust - (vacThrust - slThrust) * (pressure() / 101325).
+}
+
+//Returns mass (kg) at time t
+declare function myMass {
+    return wetMass + burnRate * t.
+}
+
+//Returns vertical acceleration (m/s^2)
+declare function av {
+    return thrust() / myMass() * sin(velTheta) - g.
+}
+
+//Returns horizontal acceleration (m/s^2)
+declare function ah {
+    return thrust() / myMass() * cos(velTheta).
+}
+
+//sum vertical acceleration for velocity (m/s)
+declare function sumVertAcc {
+    set vertVelocity to vertVelocity + (av() / frequency.
+}
+
+//sum horizontal acceleration for velocity (m/s)
+declare function sumHorizAcc {
+    set horizVelocity to horizVelocity + (ah() / frequency).
+}
+
+//arctangent sums for angle of velocity vector from horizontal (degrees)
+declare function update {
+    sumVertAcc().
+    sumHorizAcc().
+    set myAltitude to myAltitude + (vertVelocity / frequency).
+    set velTheta to arctan(vertVelocity / horizVelocity).
+}
+
+//Flight Section
+clearscreen.
+set attitude to 90.
+lock steering to heading(90, attitude).
+lock throttle to 1.0.
+set count to 5.
+until (count = 0) {
+    print count + "...".
+    set count to count - 1.
+    wait 1.
+}
+stage.
+print("Ignition").
+wait 3.
+stage.
+print("Liftoff").
+
+set liftoffTime to Time:seconds.
+lock currentTime to Time:seconds - liftoffTime.
+//simulates ascent for burn time length
+until (myAltitude > pitchAlt) {
+    update().
+    print("Pitch: " + velTheta) at (0, 9).
+    print("T+ " + t) at (0, 10).
+    set attitude to velTheta.
+    set t to t + (1 / frequency).
+    wait until currentTime > t.
+}
+
+set velTheta to 89.9.
+print("Pitchover " + vertVelocity).
+
+until (t > burnTime) {
+    update().
+    print("Pitch: " + velTheta) at (0, 9).
+    print("T+ " + t) at (0, 10).
+    set attitude to velTheta.
+    set t to t + (1 / frequency).
+    wait until currentTime > t.
+}
+
+//calculate altitude at burnout -- sum vertical acceleration (m)
+print("Altitude at cutoff: " + myAltitude).
+print("% variation: " + (altitude - myAltitude) / myAltitude * 100).
+
+//apogee calculator
+//alt to apogee: vf=0, a=g, vi=sumVertAcc vf^2=vi^2+2ady dy=-vi^2/2a
+//total alt of apogee: dy+altitude
+set myApogee to (vertVelocity^2) / (2 * g) + myAltitude
+print("Apogee altitude: " + myApogee).
+print("% variation: " + (Orbit:apoapsis - myApogee) / myApogee * 100).
--- openLoopAscent.py
+++ openLoopAscent.py
@@ -1,95 +1,88 @@
 import math
 
-#Sim parameters and data
-g = 9.8 #gravitational acceleration (m/s^2)
-t = 0 #time (sec)
-velTheta = math.pi / 2 #angle of velocity vector from horizontal (radians)
-vertVelocity = 0 #vertical velocity summed from vertical acceleration (m)
-horizVelocity = 0 #horizontal velocity summed from horizontal acceleration (m)
-altitude = 0 #altitude summed from vertVelocity (m)
-frequency = 5 #number of measurements per second (Hz)
+class Rocket:
 
-#Rocket data
-wetMass = 9714 #mass with fuel load (kg)
-dryMass = 3122 #mass without fuel load (kg)
-burnTime = 142 #burn duration (sec)
-vacThrust = 123600 #thrust produced in a vacuum (N)
-slThrust = 112900 #thrust produced at sea level (N)
-burnRate = (dryMass - wetMass) / burnTime #fuel burn rate (kg/s)
+    def __init__(self, f, m0, m1, tv, ts, bt):
+        self.frequency = 20 #update frequency (Hz)
+        self.wetMass = m0 #mass with prop load (kg)
+        self.dryMass = m1 #mass without prop load (kg)
+        self.burnTime = bt #burn duration (s)
+        self.thrustVac = tv #thrust in a vacuum (N)
+        self.thrustSL = ts #thrust at 1 atm (N)
+        self.burnRate = (m1 - m0) / bt
+        self.g = -9.8 #gravitational acceleration (m/s^2)
+        self.t = 0 #time (sec)
+        self.velTheta = math.pi / 2 #angle of velocity vector from horizontal (radians)
+        self.vertVel = 0 #vertical velocity summed from vertical acceleration (m)
+        self.horizVel = 0 #horizontal velocity summed from horizontal acceleration (m)
+        self.altitude = 0 #altitude summed from self.vertVel (m)
 
-#atmospheric pressure at altitude (Pa)
-def pressure():
-    if (altitude < 44330):
-        return 101325*(1-(2.25577*10**-5)*altitude)**5.25588
-    else:
-        return 0
+    #Returns atmospheric pressure (Pa) at altitude
+    def pressure(self):
+        if (self.altitude < 44330):
+            return 101325*(1-(2.25577*10**-5)*self.altitude)**5.25588
+        else:
+            return 0
 
-#thrust at atmospheric pressure (N)
-def thrust():
-    return vacThrust-(vacThrust-slThrust)*(pressure()/101325)
+    #thrust at atmospheric pressure (N)
+    def thrust(self):
+        return self.thrustVac-(self.thrustVac-self.thrustSL)*(self.pressure()/101325)
 
-#mass at given time t (kg)
-def mass():
-    return wetMass + burnRate * t
+    #mass at given time t (kg)
+    def mass(self):
+        return self.wetMass + self.burnRate * self.t
 
-#total acceleration prior to gravity loss at given time t (m/s^2)
-def a0():
-    return thrust() / mass()
+    #total acceleration prior to gravity loss at given time t (m/s^2)
+    def a0(self):
+        return self.thrust() / self.mass()
 
-#vertical component of acceleration including gravity loss at given time t (m/s^2)
-def av():
-    return a0() * math.sin(velTheta) - g
+    #vertical component of acceleration including gravity loss at given time t (m/s^2)
+    def av(self):
+        return self.a0() * math.sin(self.velTheta) + self.g
 
-#horizontal component of velocity at given time t (m/s^2)
-def ah():
-    return a0() * math.cos(velTheta)
+    #horizontal component of velocity at given time t (m/s^2)
+    def ah(self):
+        return self.a0() * math.cos(self.velTheta)
 
-#sum vertical acceleration for velocity at given time t (m/s)
-def sumVertAcc():
-    global vertVelocity
-    vertVelocity += (av() / frequency)
-    return vertVelocity
+    #sum vertical acceleration for velocity at given time t (m/s)
+    def sumVertAcc(self):
+        self.vertVel += (self.av() / self.frequency)
+        return self.vertVel
 
-#sum horizontal acceleration for velocity at given time t (m/s)
-def sumHorizAcc():
-    global horizVelocity
-    horizVelocity += (ah() / frequency)
-    return horizVelocity
+    #sum horizontal acceleration for velocity at given time t (m/s)
+    def sumHorizAcc(self):
+        self.horizVel += (self.ah() / self.frequency)
 
-#arctangent sums for angle of velocity vector from horizontal (degrees)
-def velTheta1():
-    return math.atan(sumVertAcc() / sumHorizAcc())
+    #arctangent sums for angle of velocity vector from horizontal (degrees)
+    def update(self):
+        self.sumVertAcc()
+        self.sumHorizAcc()
+        self.altitude += (self.vertVel / self.frequency)
+        self.velTheta = math.atan(self.horizVel / self.vertVel)
 
-simOutput = ""
-#simulates ascent for burn time length
-while t < 9:
-    velTheta = velTheta1()
-    print(velTheta * (180/math.pi))
-    altitude += (vertVelocity / frequency)
-    t += 1 / frequency
-    
-    simOutput += str(velTheta * (180/math.pi)) + ", "
+    def simulateAscent(self, pitchAlt, pitchAngle):
+        #simulates ascent for burn time length
+        hitGround = False
+        while self.altitude < pitchAlt and hitGround is False:
+            self.update()
+            if (self.altitude < 0) :
+                print("Hit ground at T+ " + str(self.t))
+                hitGround = True
+            self.t += 1 / self.frequency
 
-velTheta = (8985 * math.pi)/18000
-print("Pitchover " + str(vertVelocity))
+        velTheta = (pitchAngle * math.pi) / 18000
+        print("Pitchover " + str(self.vertVel))
 
-while t < burnTime + 1:
-    velTheta = velTheta1()
-    print(velTheta * (180/math.pi))
-    altitude += (vertVelocity / frequency)
-    t += 1 / frequency
-    
-    simOutput += str(velTheta * (180/math.pi)) + ", "
+        while self.t < self.burnTime + 1 and hitGround is False:
+            self.update()
+            if (self.altitude < 0) :
+                print("Hit ground at T+ " + str(self.t))
+                hitGround = True
+            self.t += 1 / self.frequency
 
-#calculate altitude at burnout -- sum vertical acceleration (m)
-print("Altitude at cutoff: " + str(altitude))
+    def getApogee(self):
+        return -1 * (self.vertVel ** 2) / (2 * self.g) + self.altitude
 
-#apogee calculator
-#alt to apogee: vf=0, a=g, vi=sumVertAcc vf^2=vi^2+2ady dy=-vi^2/2a
-#total alt of apogee: dy+altitude
-print("Vertical speed at cutoff: " + str(vertVelocity))
-print("Total speed at cutoff: " + str(vertVelocity/math.sin(velTheta)))
-print("Alt to apogee: " + str((vertVelocity**2)/(2*g)))
-print("Apogee altitude: " + str(((vertVelocity**2)/(2*g) + altitude)))
-
-print(simOutput)
+demo = Rocket(20, 9665, 3072, 123600, 112900, 150)
+demo.simulateAscent(20, 100000)
+print("Apogee: " + str(demo.getApogee()))
