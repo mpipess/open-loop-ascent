6a70d48fc0fbf24b522543ada63cf47183f35d0c
Michael Piper <n1k1shiden@gmail.com>
2022-02-28 14:05:50 -0500

    Clean up comments

--- AscentSim.py
+++ AscentSim.py
@@ -1,13 +1,3 @@
-#This simulator is meant to aid in finding the correct angle to which to pitch over
-#during a rocket's first stage ascent.
-#The user inputs rocket data, then runs the sim with parameters of pitchover altitude
-#(representing clearance of pad structures) and a target apogee, from which the program
-#can brute-force a pitchover angle through repeated simulation.
-#The simulation steps time forward at a user-set erval, getting acceleration from
-#mass and thrust, velocity from summation of accelerations, pitch attitude from the ratio
-#of vertical and horizontal velocities, and altitude from summation of vertical velocity.
-#Apogee calculation is via basic kinematics.
-
 import math
 
 class Rocket:
